9c5431d2032e4d4d522bb888e703c88401c321fc
Edi61 <84682915+Edi61@users.noreply.github.com>
2023-03-09 21:25:28 +0100

    Publish SingleFile Portable

---
+++ cmd/MakePublish.cmd
@@ -0,0 +1,50 @@
+@echo off
+
+set RAR_EXE="C:\Program Files\WinRAR\WinRar.exe"
+
+set ROOT_DIR=..
+set RELEASE_DIR=%ROOT_DIR%\bin\Publish
+set VERSION_FILE=%ROOT_DIR%\src\version.txt
+
+if not exist %RELEASE_DIR% (
+echo Can't find "%RELEASE_DIR%" directory!
+exit 0
+)
+
+if not exist %VERSION_FILE% (
+echo Can't find "%VERSION_FILE%" file!
+exit 0
+)
+
+set VERSION=
+for /f "delims=" %%i in ('type %VERSION_FILE%') do set VERSION=%%i
+
+set OUT_DIR=%ROOT_DIR%\out\publish
+set TMP_DIR=%ROOT_DIR%\out\publish\AntiDupl.NET-%VERSION%
+
+if not exist %OUT_DIR% mkdir %OUT_DIR%
+
+if exist %TMP_DIR% (
+echo Delete old files:
+erase %TMP_DIR%\* /q /s /f
+rmdir %TMP_DIR% /q /s
+)
+
+if not exist %TMP_DIR% mkdir %TMP_DIR%
+
+xcopy %RELEASE_DIR% %TMP_DIR% /e /h /c /i
+
+erase %TMP_DIR%\data\resources\strings\English.xml /q /s /f
+erase %TMP_DIR%\data\resources\strings\Russian.xml /q /s /f
+
+if exist %RAR_EXE% (
+%RAR_EXE% a -ep1 -s -m5 -r -sfx %OUT_DIR%\AntiDupl.NET-%VERSION%_SingleFilePortable.exe %TMP_DIR%
+certutil -hashfile %OUT_DIR%\AntiDupl.NET-%VERSION%.exe SHA256 > %OUT_DIR%\AntiDupl.NET-%VERSION%_SingleFilePortable.exe.hash.txt
+%RAR_EXE% a -afzip -ep1 -r %OUT_DIR%\AntiDupl.NET-%VERSION%_SingleFilePortable.zip %TMP_DIR%
+certutil -hashfile %OUT_DIR%\AntiDupl.NET-%VERSION%.zip SHA256 > %OUT_DIR%\AntiDupl.NET-%VERSION%_SingleFilePortable.zip.hash.txt
+) else (
+.\7-zip\7za_2201.exe a -sfx7z.sfx %OUT_DIR%\AntiDupl.NET-%VERSION%_SingleFilePortable.exe %TMP_DIR%
+certutil -hashfile %OUT_DIR%\AntiDupl.NET-%VERSION%_SingleFilePortable.exe SHA256 > %OUT_DIR%\AntiDupl.NET-%VERSION%_SingleFilePortable.exe.hash.txt
+.\7-zip\7za_2201.exe a %OUT_DIR%\AntiDupl.NET-%VERSION%_SingleFilePortable.7z .\%TMP_DIR%\*
+certutil -hashfile %OUT_DIR%\AntiDupl.NET-%VERSION%_SingleFilePortable.7z SHA256 > %OUT_DIR%\AntiDupl.NET-%VERSION%_SingleFilePortable.7z.hash.txt
+)
